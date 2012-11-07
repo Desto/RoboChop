fcb30c57e20436b0813490d67386759462efeb2f
Petteri Hyvärinen <petiera@gmail.com>
2012-11-07 10:47:11 +0200

    Result of the weekend
    In the last version from the weekend, there are new functions
    arm_motors(), disarm_motors() and calibrate(). Their function is based
    on MultiWii stick commands:
    http://code.google.com/p/multiwii/source/browse/#svn%2Fbranches%2FHamburger
    Also added a way to control the throttle, roll, pitch and yaw on-the-fly
    by sending integers through the serial connection. See loop()

--- throttle_test.ino
+++ throttle_test.ino
@@ -19,6 +19,8 @@ Servo pitch_servo;
 Servo roll_servo;
 Servo aux_servo;
 
+int static_throttle = SERVO_MIN;
+
 void setup() {
 
   Serial.begin(9600);
@@ -34,10 +36,17 @@ void setup() {
   yaw_servo.write(SERVO_MIDDLE);
   pitch_servo.write(SERVO_MIDDLE);
   roll_servo.write(SERVO_MIDDLE);
-  aux_servo.write(SERVO_MIN);
+  aux_servo.write(SERVO_MAX);
   Serial.println("servos set to zero, start throttle");
   
+  Serial.println("Calibrate");
+  calibrate();
+  arm_motors();
   
+  Serial.println("Give number: ");
+  int uselessnumber = Serial.parseInt();
+  Serial.print("Number: ");
+  Serial.println(uselessnumber);
 //  throttle_servo.write(SERVO_MAX);
 //  Serial.setTimeout(30000);
 //  Serial.println("Throttle max, write 'end' when calibrated");
@@ -47,45 +56,155 @@ void setup() {
 //  Serial.println("Throttle min, write 'end' when calibrated");
 //  Serial.findUntil("end", "\n");
   
-  throttle_servo.write(SERVO_MIN);
-  yaw_servo.write(SERVO_MAX);
+//  Serial.println("Start accelerating");
+//  for (int throttle = SERVO_MIN; throttle < SERVO_MIN + 500; throttle += 20) {
+//    Serial.println(throttle);
+//    throttle_servo.write(throttle);
+//    delay(1000);
+//  }
+//  
+//  delay(5000);
+//  
+//  Serial.println("Cool down");
+//  for (int throttle = SERVO_MIN + 500; throttle >= SERVO_MIN; throttle -= 20) {
+//    Serial.println(throttle);
+//    throttle_servo.write(throttle);
+//    delay(1000);
+//  }
+
   
-  delay(5000);
-  
-  yaw_servo.write(SERVO_MIDDLE);
-  
-  Serial.println("Start accelerating");
-  for (int throttle = SERVO_MIN; throttle < SERVO_MIN + 500; throttle += 20) {
-    Serial.println(throttle);
-    throttle_servo.write(throttle);
-    delay(1000);
-  }
-  
-  delay(5000);
-  
-  Serial.println("Cool down");
-  for (int throttle = SERVO_MIN + 500; throttle >= SERVO_MIN; throttle -= 20) {
-    Serial.println(throttle);
-    throttle_servo.write(throttle);
-    delay(1000);
-  }
-  
-  Serial.println("Minimum throttle");
-  throttle_servo.write(SERVO_MIN);
-  delay(5000);
-  Serial.println("Disarm motors");
-  yaw_servo.write(SERVO_MIN);
-  
-  Serial.println("Test if motors still run");
-  throttle_servo.write(SERVO_MIN + 300);
-  delay(2000);
-  throttle_servo.write(SERVO_MIN);
 }
 
 void loop() {
-  throttle_servo.write(SERVO_MIN + 300);
-  delay(10000);
-  
+  if (Serial.available() > 0) {
+    int value = Serial.parseInt();
+    if (value > 1000) {
+      Serial.print("New throttle: ");
+      Serial.println(value, DEC);
+      throttle_servo.write(value);
+    }
+    else if (value == 1) {
+      arm_motors();
+    }
+    else if (value == 0) {
+      disarm_motors();
+    }
+    else if (value == 2) {
+      Serial.println("Set, ROLL: 1, PITCH: 2, YAW: 3");
+      while (Serial.available() <= 0) {
+        delay(500);
+      }
+      int newvalue = Serial.parseInt();
+      Serial.print("Input: ");
+      Serial.println(newvalue);
+      if (newvalue == 1) {
+        Serial.println("ROLL MID: ");
+        while (Serial.available() <= 0) {
+          delay(500);
+        }
+        roll_servo.write(Serial.parseInt());
+        delay(500);
+        Serial.print("Roll value: ");
+        Serial.println(roll_servo.readMicroseconds());
+      }
+      else if (newvalue == 2) {
+        Serial.println("PITCH MID: ");
+        while (Serial.available() <= 0) {
+          delay(500);
+        }
+        pitch_servo.write(Serial.parseInt());
+        delay(500);
+        Serial.print("Pitch value: ");
+        Serial.println(pitch_servo.readMicroseconds());
+      }
+      else if (newvalue == 3) {
+        Serial.println("YAW MID: ");
+        while (Serial.available() <= 0) {
+          delay(500);
+        }
+        yaw_servo.write(Serial.parseInt());
+        delay(500);
+        Serial.print("Yaw value: ");
+        Serial.println(yaw_servo.readMicroseconds());
+      }
+    }
+//    else if (value == 2) {  // calibration acro gyro
+//      disarm_motors();
+//      throttle_servo.write(SERVO_MIN);
+//      yaw_servo.write(SERVO_MIN);
+//      pitch_servo.write(SERVO_MIN);
+//      roll_servo.write(SERVO_MIDDLE);
+//      delay(2000);
+//    }
+//    else if (value == 3) { // calibration stable acc
+//      disarm_motors();
+//      yaw_servo.write(SERVO_MIN);
+//      throttle_servo.write(SERVO_MAX);
+//      pitch_servo.write(SERVO_MIN);
+//      roll_servo.write(SERVO_MIDDLE);
+//    }
+//    else if (value == 4) { // trim acc pitch UP
+//      disarm_motors();
+//      yaw_servo.write(SERVO_MIDDLE);
+//      throttle_servo.write(SERVO_MAX);
+//      pitch_servo.write(SERVO_MAX);
+//      roll_servo.write(SERVO_MIDDLE);
+//    }
+//    else if (value == 5) { // trim acc pitch DOWN
+//      disarm_motors();
+//      yaw_servo.write(SERVO_MIDDLE);
+//      throttle_servo.write(SERVO_MAX);
+//      pitch_servo.write(SERVO_MIN);
+//      roll_servo.write(SERVO_MIDDLE);
+//    }
+//    else if (value == 6) { // trim acc roll LEFT
+//      disarm_motors();
+//      yaw_servo.write(SERVO_MIDDLE);
+//      throttle_servo.write(SERVO_MAX);
+//      pitch_servo.write(SERVO_MIDDLE);
+//      roll_servo.write(SERVO_MIN);
+//    }
+//    else if (value == 7) { // trim acc roll RIGHT
+//      disarm_motors();
+//      yaw_servo.write(SERVO_MIDDLE);
+//      throttle_servo.write(SERVO_MAX);
+//      pitch_servo.write(SERVO_MIDDLE);
+//      roll_servo.write(SERVO_MAX);
+//    }
+  }
+  delay(1000);
+}
+
+void calibrate() {
   throttle_servo.write(SERVO_MIN);
-  delay(10000);
+  delay(1000);
+  yaw_servo.write(SERVO_MIN);
+  delay(1000);
+  pitch_servo.write(SERVO_MIN);
+  delay(1000);
+  yaw_servo.write(SERVO_MAX);
+  delay(1000);
+  yaw_servo.write(SERVO_MIDDLE);
+  delay(1000);
+  pitch_servo.write(SERVO_MIDDLE);
+  Serial.println("Calibration ready");
+}
+
+void arm_motors() {
+  pitch_servo.write(SERVO_MIDDLE);
+  roll_servo.write(SERVO_MIDDLE);
+  throttle_servo.write(SERVO_MIN);
+  yaw_servo.write(SERVO_MAX);
+  
+  delay(3000);
+  
+  yaw_servo.write(SERVO_MIDDLE);
+  Serial.println("Motors armed");
+}
+
+void disarm_motors() {
+  Serial.println("Minimum throttle");
+  throttle_servo.write(SERVO_MIN);
+  Serial.println("Disarm motors");
+  yaw_servo.write(SERVO_MIN);
 }
